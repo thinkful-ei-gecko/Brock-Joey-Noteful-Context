b4332b1afe77f9b2b79ee074484f98b3e6c301d7
Brock Boutwell <brockboutwell@gmail.com>
2019-09-12 15:59:57 -0400

    passback to delete button

--- src/components/Context.js
+++ src/components/Context.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 
 export default React.createContext({
-    notes: [],
-    folders: [],
-    addFolder: () => {},
-  addNote: () => {},
-  deleteNote: () => {},
-})
+  notes: [],
+  folders: [],
+  deleteNote: () => {}
+});
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,15 +1,44 @@
-import React from 'react'
-import Header from './Header'
-import { Link } from 'react-router-dom'
-import Context from './Context'
-
+import React from "react";
+import Header from "./Header";
+import { Link } from "react-router-dom";
+import Context from "./Context";
 
 export default class Note extends React.Component {
   static contextType = Context;
 
+  handleDeleteButton = e => {
+    const baseUrl = "http://localhost:9090";
+
+    e.preventDefault();
+    const noteId = this.props.id;
+
+    fetch(`${baseUrl}/notes/${noteId}`, {
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json"
+      }
+    })
+    .then(response => {
+      if (!response.ok) {
+        return response.json().then(e => Promise.reject(e))
+      }
+      return response.json();
+    })
+    .then(() => {
+      this.context.deleteNote(noteId)
+    })
+    .catch(error => {
+      console.log({error})
+    })
+  };
+
   render() {
-    const note = this.context.notes.find(n => n.id === this.props.match.params.noteId)
-    const selectedFolder = this.context.folders.find(folder => folder.id === note.folderId)
+    const note = this.context.notes.find(
+      n => n.id === this.props.match.params.noteId
+    );
+    const selectedFolder = this.context.folders.find(
+      folder => folder.id === note.folderId
+    );
     return (
       <main>
         <Header />
@@ -17,9 +46,13 @@ export default class Note extends React.Component {
         <p>{note.content}</p>
         <p>{new Date(note.modified).toDateString()}</p>
         <p>{selectedFolder.name}</p>
-        <button><Link to='/'>Go Back</Link></button>
-        <button>Delete</button>
+        <button>
+          <Link to="/">Go Back</Link>
+        </button>
+        <button 
+        type="button" 
+        onClick={this.handleDeleteButton}>Delete</button>
       </main>
-    )
+    );
   }
 }
